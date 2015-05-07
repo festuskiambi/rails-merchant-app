7d8c441ffa2c0fcd0bdd36b0dcf33e731f67d4b1
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-05-07 07:40:36 +0300

    updated the helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,11 @@ module ProductsHelper
 	def print_price(price)
   number_to_currency(price, :unit => "ksh ", :separator => ".", :delimiter => ".")
 end
+def print_stock(stock)
+  if stock > 0
+  
+  else
+  
+  end
+end
 end
